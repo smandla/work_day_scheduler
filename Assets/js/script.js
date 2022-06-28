04eee7d32ed74ad2d01c0c94e1f68eb6870c5def
smandla <35283559+smandla@users.noreply.github.com>
2022-06-28 14:47:21 -0700

    Can change background of textarea

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -2,18 +2,18 @@ var container = $(".container");
 var currentDay = $("#currentDay");
 var section = $("<section>");
 var formattedDate = moment().format("MMMM DD, YYYY");
-
+var hour = moment().hour();
 //9 - 5 object
 var time_blocks = [
   { time: "9:00", am: true, digit: 9, note: "" },
   { time: "10:00", am: true, digit: 10, note: "" },
   { time: "11:00", am: true, digit: 11, note: "" },
   { time: "12:00", am: false, digit: 12, note: "" },
-  { time: "1:00", am: false, digit: 1, note: "" },
-  { time: "2:00", am: false, digit: 2, note: "" },
-  { time: "3:00", am: false, digit: 3, note: "" },
-  { time: "4:00", am: false, digit: 4, note: "" },
-  { time: "5:00", am: false, digit: 5, note: "" },
+  { time: "1:00", am: false, digit: 13, note: "" },
+  { time: "2:00", am: false, digit: 14, note: "" },
+  { time: "3:00", am: false, digit: 15, note: "" },
+  { time: "4:00", am: false, digit: 16, note: "" },
+  { time: "5:00", am: false, digit: 17, note: "" },
 ];
 
 function init() {
@@ -34,6 +34,7 @@ function displayTimeBlocks() {
   console.log(time_blocks.length);
   for (let i = 0; i < time_blocks.length; i++) {
     console.log(time_blocks[i].time);
+    var digit = time_blocks[i].digit;
     var div = $("<div>")
       .addClass("row time-block")
       .attr("id", i)
@@ -43,9 +44,21 @@ function displayTimeBlocks() {
       .text(time_blocks[i].time)
       .appendTo(div);
     var textarea = $("<textarea>").addClass("col-8").appendTo(div);
+    changeBackgroundColor(textarea, digit);
     var button = $("<button>").addClass("col-1 saveBtn").appendTo(div);
     var iEL = $("<i>").addClass("fas fa-save").appendTo(button);
   }
   section.appendTo(container);
 }
+
+const changeBackgroundColor = (textarea, digit) => {
+  console.log(hour, digit);
+  if (hour === digit) {
+    textarea.addClass("present");
+  } else if (digit < hour) {
+    textarea.addClass("past");
+  } else {
+    textarea.addClass("future");
+  }
+};
 init();
