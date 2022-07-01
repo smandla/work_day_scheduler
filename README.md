a18803d69c10efa29f7a160e77e6bdd558311b4f
smandla <35283559+smandla@users.noreply.github.com>
2022-07-01 11:10:52 -0700

    Add comments to js file

--- README.md
+++ README.md
@@ -14,21 +14,7 @@
 
 ## Description
 
-```md
-GIVEN I am using a daily planner to create a schedule
-WHEN I open the planner
-THEN the current day is displayed at the top of the calendar
-WHEN I scroll down
-THEN I am presented with timeblocks for standard business hours
-WHEN I view the timeblocks for that day
-THEN each timeblock is color coded to indicate whether it is in the past, present, or future
-WHEN I click into a timeblock
-THEN I can enter an event
-WHEN I click the save button for that timeblock
-THEN the text for that event is saved in local storage
-WHEN I refresh the page
-THEN the saved events persist
-```
+A work day schedule where the data persists and the user can save anything they would like within each work hour time block.
 
 ## Technologies
 
--- script.js
+++ script.js
@@ -1,116 +1,130 @@
-// console.log(data);
-console.log("hello!");
+//formatted date using moment
 var formattedDate = moment().format("dddd, MMMM DD, YYYY");
-console.log(formattedDate);
+var hour = moment().hour();
+//grab tags from html
 var container = $(".container");
 var currentDay = $("#currentDay");
 var section = $("<section>");
+
 var data;
 var uData;
-var hour = moment().hour();
-//9 - 5 object
+
+//an array of objects that holds the info and time for each time block
 var time_blocks = [
-  { time: "9:00", am: true, digit: 9, note: "" },
-  { time: "10:00", am: true, digit: 10, note: "" },
-  { time: "11:00", am: true, digit: 11, note: "" },
-  { time: "12:00", am: false, digit: 12, note: "" },
-  { time: "1:00", am: false, digit: 13, note: "" },
-  { time: "2:00", am: false, digit: 14, note: "" },
-  { time: "3:00", am: false, digit: 15, note: "" },
-  { time: "4:00", am: false, digit: 16, note: "" },
-  { time: "5:00", am: false, digit: 17, note: "" },
+  { time: "9:00", digit: 9, note: "" },
+  { time: "10:00", digit: 10, note: "" },
+  { time: "11:00", digit: 11, note: "" },
+  { time: "12:00", digit: 12, note: "" },
+  { time: "1:00", digit: 1, note: "" },
+  { time: "2:00", digit: 2, note: "" },
+  { time: "3:00", digit: 3, note: "" },
+  { time: "4:00", digit: 4, note: "" },
+  { time: "5:00", digit: 5, note: "" },
 ];
 
+/**
+ * Function that runs when the page is loaded.
+ */
 function init() {
+  //display current date
   currentDay.text(formattedDate);
+  //grab data from local storage or create an empty dataset
   if (JSON.parse(localStorage.getItem("work_day_scheduler"))) {
     data = JSON.parse(localStorage.getItem("work_day_scheduler"));
   } else {
     data = time_blocks;
   }
-  // data = JSON.parse(localStorage.getItem("work_day_scheduler")) || time_blocks;
-  // console.log(time_blocks[i].time, data);
-  console.log(data);
-  uData = data;
-  console.log(uData);
 
-  //show one row of the time blocks with 3 columns
-  /**
-     *  <div class="row time-block">
-          <span class="col-2 hour">12:00</span>
-          <textarea class="col-8"></textarea>
-          <button class="col-1 saveBtn"></button>
-        </div>
-     */
+  //copy data to uData
+  uData = data;
+
+  //call the function to display time blocks
   displayTimeBlocks();
 }
 
+/**
+ * Function to display time blocks
+ */
 function displayTimeBlocks() {
-  // console.log(time_blocks.length);
+  //loop through for loop to create the time blocks
   for (let i = 0; i < time_blocks.length; i++) {
-    // console.log(time_blocks[i].time);
+    //grab the digit version of the time for bg
     var digit = time_blocks[i].digit;
+    //create a div tag with a class and id
     var div = $("<div>")
       .addClass("row time-block")
       .attr("id", i)
       .appendTo(section);
+
+    //create a span tag with a class and text of time
     var span = $("<span>")
       .addClass("col-2 hour")
       .text(time_blocks[i].time)
       .appendTo(div);
+    //create a text area for user input with a class and unique id
     var textarea = $("<textarea>")
       .attr("id", "time_id_" + i)
       .addClass("col-8")
       .appendTo(div);
+
+    //call function to change background color
     changeBackgroundColor(textarea, digit);
 
+    //if there is data in local storage, data will stay on page on refresh
     if (data) {
+      //if the data at this current index doesn't have an empty string for a note
       if (data[i].note) {
         textarea.text(data[i].note);
       }
     }
 
+    //create a button with a class and unique id
     var button = $("<button>")
       .addClass("col-1 saveBtn")
       .appendTo(div)
       .attr("id", i)
+      //when the button is clicked, data inside the textarea is saved inside local storage
       .on("click", function (e) {
         e.preventDefault();
+        //adds textarea val to data object for local storage
         uData[i].note = $("#time_id_" + i).val();
 
-        console.log($("#time_id_" + i).val());
-
-        console.log(time_blocks);
-
-        localStorage.setItem(
-          time_blocks[i].time,
-          JSON.stringify(time_blocks[i].note)
-        );
-
+        //only updates the the specific note at the specific index, so previous data doesn't get erased
         var newData = saveTask(i, uData[i].note);
-        console.log(newData);
+
+        //sets local storage with the new data stringified
         localStorage.setItem("work_day_scheduler", JSON.stringify(newData));
       });
+    //create that save icon for the button
     var iEL = $("<i>").addClass("fas fa-save").appendTo(button);
   }
-
+  //add each time block section to the entire container
   section.appendTo(container);
 }
 
+/**
+ * Function that takes in two arguments (i, note)
+ * to update the data only by its id and not the entire object
+ */
 const saveTask = (i, note) => {
+  // map() loops through the data
   const updatedData = uData.map((obj, index) => {
+    //compare the current index with the i index from the onclick
     if (index === i) {
-      console.log(i, index);
+      //update the note at that obj[i]
       return { ...obj, note: note };
     }
-
+    //return the updated obj[i]
     return obj;
   });
+  //return the updated data
   return updatedData;
 };
 
+/**
+ * Function that changes the backround color to all the text area blocks
+ */
 const changeBackgroundColor = (textarea, digit) => {
-  // console.log(hour, digit);
   if (hour === digit) {
     textarea.addClass("present");
   } else if (digit < hour) {
@@ -119,5 +133,5 @@ const changeBackgroundColor = (textarea, digit) => {
     textarea.addClass("future");
   }
 };
+//calls the init function that starts off the script
 init();
-// localStorage.removeItem("work_day_scheduler");
